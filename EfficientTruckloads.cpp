bda5e6f35efc69950fbc5fd06ceb6eb7a62147a2
a1793854 <64885118+a1793854@users.noreply.github.com>
2022-08-24 05:31:07 +0930

    Add files via upload

--- EfficientTruckloads.cpp
+++ EfficientTruckloads.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 #include <iostream>
 #include <vector>
+#include <cstring>
 
 EfficientTruckloads::EfficientTruckloads(){
 
@@ -12,27 +13,21 @@ EfficientTruckloads::EfficientTruckloads(){
 
 int EfficientTruckloads::numTrucks(int numCrates, int loadSize){
   
-  // inititalize table of vetcors with row and columns
-  static vector<vector<int>> table(ROW);
-  for (int i = 0 ; i < ROW; i++) {
-
-    table[i].resize(COLUMN);
-        
-  }
+  // inititalize table 
+  static int table[ROW][COLUMN];
 
   // fill vectors with -1;
- for(int i=0; i<ROW; i++){
-    for(int j=0; j<COLUMN; j++){    
+  memset(table, -1, sizeof(table));
 
-      table[i][j]= -1;
-
-    }
-  }
-
-
-  if (numCrates <= loadSize) { // base case
+ if (numCrates <= loadSize) { // base case
 
     table[numCrates-1][loadSize-1] = 1;
+
+  }
+
+  if (table[numCrates-1][loadSize-1] != -1) { // check if value already there
+
+    return table[numCrates-1][loadSize-1];
 
   }else if (numCrates % 2 != 0) { // recursive cases
 
@@ -51,13 +46,22 @@ return table[numCrates-1][loadSize-1];
 }
 
 
+/*   // inititalize table of vetcors with row and columns
+  static vector<vector<int>> table(ROW);
+  for (int i = 0 ; i < ROW; i++) {
 
+    table[i].resize(COLUMN);
+        
+  }
 
+  // fill vectors with -1;
+ for(int i=0; i<ROW; i++){
+    for(int j=0; j<COLUMN; j++){    
 
+      table[i][j]= -1;
 
-
-
-
+    }
+  } */
 
 
 
