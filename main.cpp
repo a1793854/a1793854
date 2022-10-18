c83f6cce77e9f7573d3ff9fc6ad488534f35b46a
a1793854 <64885118+a1793854@users.noreply.github.com>
2022-10-18 15:25:45 +1030

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,79 +1,153 @@
-#include "Node.h"
-#include "LinkedList.h"
+#include <algorithm>
 #include <iostream>
+#include <string>
+#include <stack>
 using namespace std;
 
-int main(void) {
+// CONVERT PREFIX TO INFIX
+// Function to check if character is operator
+bool isOperator(char a) {
 
-    int i;
-    string func;
+  switch (a) {
+  case '+':
+    return true;
+  case '-':
+    return true;
+  case '/':
+    return true;
+  case '*':
+    return true;
+  default:
+    return false;
+  }
 
-    int nums[100];
-
-    for(i = 0; cin >> func; i++){   
-        if(func[0] < '0' || func[0] > '9')
-            break;
-
-        
-        nums[i] = stoi(func);
-    }
-
-    LinkedList *list = new LinkedList(nums, i);
-
-    switch(func[0]){
-
-        case 'A':
-        switch (func[1])
-        {
-            case 'F':
-            cin >> nums[0];
-            list->addFront(nums[0]);
-            break;
-
-            case 'E':
-            cin >> nums[0];
-            list->addEnd(nums[0]);
-            break;
-
-            case 'P':
-            cin >> nums[0] >> nums[1];
-            list->addAtPosition(nums[0], nums[1]);
-            break;
-
-        }
-        break;
-
-        case 'S':
-        cin >> nums[0];
-        list->search(nums[0]);
-        break;
-
-        case 'D':
-        switch (func[1])
-        {
-            case 'F':
-            list->deleteFront();
-            break;
-
-            case 'E':
-            list->deleteEnd();
-            break;
-
-            case 'P':
-            cin >> nums[0];
-            list->deletePosition(nums[0]);
-            break;
-
-        }
-        break;
-
-        case 'G':
-        cin >> nums[0];
-        list->getItem(nums[0]);
-        break;
-    }
-
-    list->printItems();
-    cout << endl;
 }
+ 
+// Convert string prefix to infix expressions
+string preToInfix(string expression) {
 
+  stack<string> new_str;
+  stack<int> value;
+
+  // Expression length
+  int length = expression.size();
+
+  // Operator and operand counter
+  int operator_count = 0;
+  int operand_count = 0;
+
+  for (int k = 0; k < length; k++){
+
+    if(isOperator(expression[k]) == true){
+      operator_count++;
+    }else if(isdigit(expression[k]) == 1){
+      operand_count++;
+    }
+
+  }
+  
+  // Check if expression is valid (if total number of operands is not one more than the number of operators)
+  if(operand_count != operator_count + 1){
+
+    cout << "\n" << operand_count << operator_count;
+    return "Error";
+
+  }
+
+  // Remove all spaces from string
+  expression.erase(remove(expression.begin(), expression.end(), ' '), expression.end());
+
+  // Read reverse
+  for (int i = length - 1; i >= 0; i--) {
+
+      // If not operator, convert operand to digit and push operand to value 
+      if (isOperator(expression[i]) == false){
+
+        value.push(expression[i] - '0');
+
+      }else { // If operator
+      
+          // Pop two operands from value
+          int operand_1 = value.top();
+          value.pop();
+          int operand_2 = value.top();
+          value.pop();
+
+          // Evaulate depending on operator
+          switch (expression[i]) {
+          case '+':
+              value.push(operand_1 + operand_2);
+              break;
+          case '-':
+              value.push(operand_1 - operand_2);
+              break;
+          case '*':
+              value.push(operand_1 * operand_2);
+              break;
+          case '/':
+              value.push(operand_1 / operand_2);
+              break;
+          }
+      }
+  }
+ 
+  // Convert the value to string
+  string val = to_string(value.top());
+
+  // Read reverse
+  for (int j = length - 1; j >= 0; j--) {
+
+    // Check if operator and not the end 
+    if (isOperator(expression[j]) && j != 0) {
+ 
+      // Pop two operands from value
+      string op1 = new_str.top();   new_str.pop();
+      string op2 = new_str.top();   new_str.pop();
+ 
+      // Concatenate operator and operands
+      string temp = "(" + op1 + ' ' + expression[j] + ' ' + op2 + ")";
+ 
+      // Push concatenation to stack
+      new_str.push(temp);
+
+    }
+    
+    // If is operator and is the end
+    else if (isOperator(expression[j]) && j == 0) { 
+ 
+      // Pop two operands from value
+      string op1 = new_str.top();   new_str.pop();
+      string op2 = new_str.top();   new_str.pop();
+ 
+      // Concatenate operator, operands, and value
+      string temp = op1 + ' ' + expression[j] + ' ' + op2 + " = " + val;
+ 
+      // Push concatenation to stack
+      new_str.push(temp);
+
+    }
+ 
+    // If operand
+    else {
+ 
+      // Push the operand to stack
+      new_str.push(string(1, expression[j]));
+    }
+
+  }
+
+  // Return converted stack
+  return new_str.top();
+
+}
+ 
+int main() {
+  //string pre_exp = "* 3 - 5 + 4 3";
+  
+  string pre_exp;
+
+  getline(cin, pre_exp);
+
+  cout << preToInfix(pre_exp);
+
+}
